f71741501db3096ade3d0098ac59027c59638f61
Santiago Prada <sanprabet@gmail.com>
2020-07-19 22:12:35 -0500

    Se escribió el html base

---
+++ puntuaciones.js
@@ -0,0 +1,15 @@
+const userName = document.querySelector('[data-input-nombre]')
+const
+const onClick_userName = document.querySelector('[data-button-agregarUsuario]')
+const onClick_Finalizar = document.querySelector('[data-button-finalizar]')
+
+class user {
+    constructor(n) {
+        this.name = n
+        this.score = 0
+    }
+
+    graficar() {
+
+    }
+}
